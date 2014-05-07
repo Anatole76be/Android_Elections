8517e7317be4a7f2f82df7025ed324416245d74d
Anatole <Anatole@Anatole-HP>
2014-05-07 21:10:14 +0200

    Base de Travail Android Elections

---
+++ A_Elections/gen/com/example/a_elections/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.a_elections;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ A_Elections/src/com/example/a_elections/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.a_elections;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+	
+	Button bouton;
+	TextView texte;
+	TextView reponse;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		bouton = (Button)findViewById(R.id.bouton);
+		texte = (TextView) findViewById(R.id.texte);
+		reponse = (TextView) findViewById(R.id.reponse);
+		
+		OnClickListener clickListener = new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				texte.setText("Coucou");
+				new MyAsyncTask().execute("A");
+			}
+		};
+		bouton.setOnClickListener(clickListener);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	//Async TASK
+	private class MyAsyncTask extends AsyncTask<String, Integer, Double>{
+		
+		
+		 
+		@Override
+		protected Double doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			postData(params[0]);
+			return null;
+		}
+ 
+ 
+		public void postData(String valueIWantToSend) {
+			// Create a new HttpClient and Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("http://109.88.27.206/ANDROID/vote.php");
+ 
+			try {
+				// Add your data
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+				nameValuePairs.add(new BasicNameValuePair("NOM", valueIWantToSend));
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+ 
+				// Execute HTTP Post Request
+				HttpResponse response = httpclient.execute(httppost);
+				//reponse.setText(response.toString());
+				
+ 
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+		}
+ 
+	}
+
+
+}
