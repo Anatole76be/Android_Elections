753975eea7a43f28ada2cf249c4571a94c46b6fd
Lemaitre Olivier <Anatole.be@gmail.com>
2014-05-09 22:31:30 +0200

    Update main activity
    Affiche la liste JSON renvoyée par getListe.php lorsque que l'on appuie sur le bouton voter.

--- A_Elections/src/com/example/a_elections/MainActivity.java
+++ A_Elections/src/com/example/a_elections/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.a_elections;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -12,22 +16,27 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	
 	Button bouton;
 	TextView texte;
-	TextView reponse;
+	//TextView reponse;
 	
 
 	@Override
@@ -37,7 +46,8 @@ public class MainActivity extends ActionBarActivity {
 		
 		bouton = (Button)findViewById(R.id.bouton);
 		texte = (TextView) findViewById(R.id.texte);
-		reponse = (TextView) findViewById(R.id.reponse);
+		//reponse = (TextView) findViewById(R.id.reponse);
+
 		
 		OnClickListener clickListener = new OnClickListener() {
 			
@@ -45,7 +55,9 @@ public class MainActivity extends ActionBarActivity {
 			public void onClick(View v) {
 
 				texte.setText("Coucou");
-				new MyAsyncTask().execute("A");
+				new MyAsyncTask().execute();
+				
+				
 			}
 		};
 		bouton.setOnClickListener(clickListener);
@@ -71,25 +83,72 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
-	//Async TASK
-	private class MyAsyncTask extends AsyncTask<String, Integer, Double>{
-		
-		
 		 
+ 
+	//Async TASK
+	private class MyAsyncTask extends AsyncTask<String, Integer, String>{
+		
+		TextView reponse = (TextView)findViewById(R.id.reponse);
+		
 		@Override
-		protected Double doInBackground(String... params) {
+		protected String doInBackground(String... params) {
 			// TODO Auto-generated method stub
-			postData(params[0]);
-			return null;
+			//postData(params[0]);
+			String data =  getData();
+			return data;
 		}
  
+		
+		public String getData() {
+	        String result ="";
+	        InputStream isr = null;
+	        try {
+	            HttpClient httpclient = new DefaultHttpClient();
+	            HttpPost post = new HttpPost("http://109.88.27.206/ANDROID/getListe.php");
+	            HttpResponse repons = httpclient.execute(post);
+	            HttpEntity enty = repons.getEntity();
+	            isr=enty.getContent();
+
+	            Log.d("http", "ok");
+	        } catch (Exception e) {
+	            Log.d("Error", "http error");
+	            
+	        }
+
+	        try {
+	            BufferedReader reader = new BufferedReader(new InputStreamReader(isr,"UTF-8"));
+	            StringBuilder sb = new StringBuilder();
+	            String line = null;
+	                while ((line = reader.readLine()) != null) {
+	                    sb.append(line+"\n");
+	                }
+	                isr.close();
+	                result=sb.toString();
+	                Log.d("result", result);
+	        } catch (Exception e) {
+	            Log.d("Error", "converting Error");
+	        }
+
+
+	        try {
+	            JSONArray jArray =  new JSONArray(result);  
+	            String s="";
+	            for (int i = 0; i < jArray.length(); i++) {
+	                JSONObject json = jArray.getJSONObject(i);  
+	                s =s+"ID :"+json.getInt("ID")+"\n";
+	            }     
+
+	        } catch (Exception e) {
+	            Log.d("Error", "Errof"+e.toString());
+	        }
+			return result;
+	    }
  
 		public void postData(String valueIWantToSend) {
+			
 			// Create a new HttpClient and Post Header
 			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost("http://monIP/ANDROID/vote.php");
+			HttpPost httppost = new HttpPost("http://109.88.27.206/ANDROID/vote.php");
  
 			try {
 				// Add your data
@@ -98,8 +157,7 @@ public class MainActivity extends ActionBarActivity {
 				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
  
 				// Execute HTTP Post Request
-				HttpResponse response = httpclient.execute(httppost);
-				//reponse.setText(response.toString());
+				HttpResponse response = httpclient.execute(httppost);				
 				
  
 			} catch (ClientProtocolException e) {
@@ -107,9 +165,18 @@ public class MainActivity extends ActionBarActivity {
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 			}
-		}
+			
  
 	}
+		
+		
+		 @Override
+		    protected void onPostExecute(String  result) {  // result is data returned by doInBackground
+		        super.onPostExecute(result);
+		        reponse.setText(result.toString());
+		        
+		    }
 
-
+	}
+	
 }
